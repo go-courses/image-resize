4a558efc8ea6e14e8e04579207890093f3db2b3a
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-03-03 12:46:36 +0600

    Удалил два роута, вообще не нужны были

--- routes.go
+++ routes.go
@@ -3,38 +3,25 @@ package main
 import "net/http"
 
 type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 var routes = Routes{
-    Route{
-        "Index",
-        "GET",
-        "/",
-        Index,
-    },
-
-    Route{
-        "Images",
-        "GET",
-        "/images",
-        Images,
-    },
-    Route{
-        "Images",
-        "POST",
-        "/images",
-        UploadImage,
-    },
-    Route{
-        "imageShow",
-        "GET",
-        "/images/{imageId}",
-        ResizeImage,
-    },
+	Route{
+		"Images",
+		"GET",
+		"/images",
+		Images,
+	},
+	Route{
+		"Images",
+		"POST",
+		"/images",
+		UploadImage,
+	},
 }
